30ffa4f302578a87ea958bb736466fbed9da8326
lilyxia99 <lilyxia999@gmail.com>
2024-12-15 08:23:21 -0500

    test portfolio page

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -14,12 +14,13 @@ function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
 
 PageTitle.css = `
 .page-title {
+  color: transparent; /* Makes the text invisible */
   margin: 0;
   margin-top:20px;
   background-image: url("https://leileixia.online/images/My-name-as-logo.gif");
   background-position:center;
   background-size:contain;
-     min-height: 150px;
+  min-height: 150px;
   background-repeat:no-repeat;
   text-shadow: 5px 5px 5px #558ABB;
 }
